be75e5fbc09152d2318e512b8394e591e0487aaf
quan-le <31929073+quan-le@users.noreply.github.com>
2025-02-27 17:39:34 +1100

    Adding javascript and api key to the movie database

---
+++ script.js
@@ -0,0 +1,5 @@
+const APILINK ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a983340ce925cbebcf4513837750e2bc&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+    //IMG_Path is the root path for all image
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=a983340ce925cbebcf4513837750e2bc&query=";
+
